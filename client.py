25a819bca1fa49e3097041b79a9b7a75aea0bc77
Jacob Burritt <burritjr@clarkson.edu>
2019-10-30 14:13:42 -0400

    Added client & server files

---
+++ client.py
@@ -0,0 +1,102 @@
+import socket
+import platform
+import re
+import sys
+import os
+import threading
+from queue import Queue
+
+# GLOBAL VARIABLES
+NUMBER_THREADS = 2
+JOB_NUMBERS = [0,1]
+
+queue = Queue()
+
+client = None
+address = None
+
+isDone = False
+
+###################################################################################################
+
+# THREAD HANDLING FUNCTIONS
+def createWorkers():
+    for _ in range(NUMBER_THREADS):
+        thr = threading.Thread(target=work)
+        thr.daemon = True
+        thr.start()
+
+def createJobs():
+    for x in JOB_NUMBERS:
+        queue.put(x)
+    queue.join()
+
+def work(): # Handle next job in the job queue, 0 Accepts new connections, 1-4 Handles clients
+    while True:
+        x = queue.get()
+        if x == 0:
+            handleInput()
+        if x == 1:
+            handleOutput()
+        queue.task_done()
+
+###################################################################################################
+
+def handleInput():
+    while True:
+        if isDone == False:
+            imsg = skt.recv(1024).decode('ascii')
+            print(imsg)
+
+def handleOutput():
+    HELLO(skt)
+    while True:
+        omsg = input() + '\n'
+        if omsg == 'QUIT\n':
+            QUIT(skt)
+        else:
+            skt.send(omsg.encode())
+
+###################################################################################################
+
+def HELLO(skt):
+    version = '1.0.1,'
+    system = platform.platform() + ','
+    program = 'Python,'
+    author = 'Jacob Burritt'
+    omsg = 'HELLO ' + version + system + program + author + '\n'
+    print(omsg)
+    skt.send(omsg.encode())
+
+def QUIT(skt):
+    global isDone
+    omsg = 'QUIT\n'
+    skt.send(omsg.encode('ascii'))
+    imsg = skt.recv(1024).decode('ascii')
+    print(imsg)
+    skt.close()
+    isDone = True
+    print('Connection Closed.\n')
+    os._exit(0)
+
+###################################################################################################
+
+if len(sys.argv) < 2:
+    print('ERROR: Please follow the following formats:')
+    print('python3 client.py <IP Address>')
+    print('python3 client.py <IP Address> <Port>\n')
+    os._exit(0)
+
+skt = socket.socket()
+
+ip = sys.argv[1]
+
+port = 9000
+if len(sys.argv) > 2:
+    port = int(sys.argv[2])
+print(ip, port)
+
+skt.connect((ip, port))
+
+createWorkers()
+createJobs()
---
+++ server.py
@@ -0,0 +1,155 @@
+import socket
+import platform
+import re
+import sys
+import threading
+from queue import Queue
+
+# GLOBAL VARIABLES
+MAX_PLAYERS = 4
+JOB_NUMBERS = [i for i in range(MAX_PLAYERS+1)]
+numPlayers = 0
+playerNames = ['Client' + str(i+1) for i in range(MAX_PLAYERS)]
+
+queue = Queue()
+clients = []
+addresses = []
+
+###################################################################################################
+
+# PROTOCOL FUNCTIONS
+def HELLO(skt):
+    version = '1.0.1,'
+    system = platform.platform() + ','
+    program = 'Python,'
+    author = 'Jacob Burritt'
+    omsg = 'HELLO ' + version + system + program + author + '\n'
+    print(omsg)
+    skt.send(omsg.encode('ascii'))
+
+def QUIT(skt):
+    print('Connection Closed.\n')
+    omsg = 'GOODBYE\n'
+    skt.send(omsg.encode('ascii'))
+    skt.close()
+    exit()
+
+def OK(skt, msg):
+    omsg = 'OK ' + msg + '\n'
+    skt.send(omsg.encode('ascii'))
+    print(omsg)
+
+def NOK(skt, msg):
+    omsg = 'NOK ' + msg + '\n'
+    print(omsg)
+    skt.send(omsg.encode('ascii'))
+
+def USERSET(skt, newName, clientNum):
+    if(newName in playerNames):
+        NOK(skt, 'Name is already taken.')
+    else:
+        oldName = playerNames[clientNum]
+        playerNames[clientNum] = newName
+        for i in range(numPlayers):
+            USERCHANGE(clients[i], newName, oldName)
+
+def USERCHANGE(skt, newName, oldName):
+    omsg = 'USERCHANGE ' + oldName + ' ' + newName + '\n'
+    print(omsg)
+    skt.send(omsg.encode('ascii'))
+
+def USERJOIN(clientNum):
+    omsg = 'USERJOIN ' + playerNames[clientNum-1] + '\n'
+    print(omsg)
+    for i in range(numPlayers):
+        clients[i].send(omsg.encode('ascii'))
+
+
+###################################################################################################
+
+# SOCKET HANDLING FUNCTIONS
+def acceptConnections():
+    global numPlayers
+    for client in clients:
+        client.close()
+    del clients[:]
+    del addresses[:]
+    while True:
+        client, address = skt.accept()
+        clients.append(client)
+        addresses.append(address)
+        numPlayers += 1
+        print('Got connection from ' + address[0] + '\n')
+
+def createSocket():
+    global port
+    global skt
+    port = 9000
+    if(len(sys.argv) > 1):
+        port = int(sys.argv[1])
+    skt = socket.socket()
+    print ('Socket successfully created.\n')
+    skt.bind(('', port))
+    print('Socket binded to %s' % (port) + '\n')
+    skt.listen(5)
+    print ('Socket is listening...\n')
+
+###################################################################################################
+
+# THREAD HANDLING FUNCTIONS
+def createWorkers():
+    for _ in range(MAX_PLAYERS):
+        thr = threading.Thread(target=work)
+        thr.daemon = True
+        thr.start()
+
+def createJobs():
+    for x in JOB_NUMBERS:
+        queue.put(x)
+    queue.join()
+
+def work(): # Handle next job in the job queue, 0 Accepts new connections, 1-4 Handles clients
+    while True:
+        x = queue.get()
+        if x == 0:
+            createSocket()
+            acceptConnections()
+        if x == 1:
+            handleInput(1)
+        if x == 2:
+            handleInput(2)
+        if x == 3:
+            handleInput(3)
+        if x == 4:
+            handleInput(4)
+        queue.task_done()
+
+def handleInput(i): # Function to handle each individual client's inputs to the server.
+    global numPlayers
+    while True:
+        if numPlayers < i:
+            continue
+        HELLO(clients[i-1])
+        imsg = clients[i-1].recv(1024).decode('ascii')
+        print(imsg + '\n')
+        if not imsg.startswith('HELLO '):
+            print('GOODBYE Client sent invalid response. Connection closed.\n')
+            QUIT(clients[i-1])
+        OK(clients[i-1], 'Scrabble Server at your service.')
+        break
+    while True:
+        imsg = clients[i-1].recv(1024).decode('ascii')
+        if (imsg.rstrip() == 'QUIT'):
+            QUIT(clients[i-1])
+        elif (imsg.split()[0] == 'USERSET'):
+            name = ' '.join(imsg.split()[1:])
+            USERSET(clients[i-1], name, i-1)
+            OK(clients[i-1], 'Name set!')
+        else:
+            print(playerNames[i-1] + ' says:', imsg)
+
+###################################################################################################
+
+# START SERVER THREADS
+createWorkers()
+createJobs()
